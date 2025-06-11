217cea053e1aa8cf7be05b7b7eae114b62ad0c40
Felix Everett <felixreverett@gmail.com>
2025-06-11 21:26:41 +0100

    dedicated projects data

---
+++ src/lib/data/projects.js
@@ -0,0 +1,46 @@
+export const projects = [
+    {
+      id: 1,
+      imageSrc: '/images/wildfawn-app-screenshot.webp',
+      imageAlt: 'Screenshot of wildfawn',
+      title: 'wildfawn SEO Crawler',
+      description: 'Open-Source lightweight SEO web crawler built in Go. I\'m currently developing a frontend using Svelte and Wails.',
+      technologies: ['golang', 'svelte', 'javascript', 'wails'],
+      categories: ['web', 'backend', 'desktop apps'],
+      projectLink: '/portfolio/wildfawn',
+      githubLink: 'https://github.com/felixreverett/wildfawn'
+    },
+    {
+      id: 2,
+      imageSrc: '/images/isola-app-screenshot.webp',
+      imageAlt: 'Screenshot of isola',
+      title: 'Isola',
+      description: 'An open-world exploration game built from the ground up in C# and OpenGL.',
+      technologies: ['csharp', 'opengl'],
+      categories: ['game development', 'graphics', 'desktop apps'],
+      projectLink: '/portfolio/isola',
+      githubLink: 'https://github.com/felixreverett/isola'
+    },
+    {
+      id: 3,
+      imageSrc: '/images/aoc-screenshot.webp',
+      imageAlt: 'Screenshot of Advent of Code',
+      title: 'Advent Of Code',
+      description: 'My JavaScript and Go solutions to the popular Advent Of Code DSA puzzles.',
+      technologies: ['javascript', 'golang'],
+      categories: ['algorithms', 'javascript'],
+      projectLink: '/portfolio/aoc',
+      githubLink: 'https://github.com/felixreverett/AdventOfCode2024'
+    },
+    {
+      id: 4,
+      imageSrc: '/images/aoc-screenshot.webp',
+      imageAlt: 'Screenshot of felixreverett website',
+      title: 'felixreverett.com',
+      description: 'My personal website and portfolio showcase, written in JavaScript and Svelte.',
+      technologies: ['javascript', 'svelte'],
+      categories: ['web', 'javascript'],
+      projectLink: '/portfolio/aoc',
+      githubLink: 'https://github.com/felixreverett/felixreverett.github.io'
+    }
+  ];
