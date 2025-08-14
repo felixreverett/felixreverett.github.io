e274480b1aec473a259cd44f42f4214c020ef3d0
Felix Everett <felixreverett@gmail.com>
2025-08-14 21:51:58 +0100

    new initial blog layout

---
+++ src/lib/layouts/BlogLayout.svelte
@@ -0,0 +1,12 @@
+<!-- Reusable layout component for portfolio pages -->
+ <div class="portfolio-layout">
+    <slot />
+ </div>
+
+<style>
+    .portfolio-layout {
+        max-width: 800px;
+        margin: 0 auto;
+        text-align: justify;
+    }
+</style>
