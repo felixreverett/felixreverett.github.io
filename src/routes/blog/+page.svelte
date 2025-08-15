f9f1da57ed87bcaaf24d10c97ec6ec43d103df8f
Felix Everett <felixreverett@gmail.com>
2025-08-15 21:45:19 +0100

    blog route prep

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,14 @@
+<svelte:head>
+  <title>Blog | felixreverett</title>
+  <meta name="description" content="My Full Blog Page." />
+</svelte:head>
+
+<div class="page-wrapper">
+	<div class="hero-section">
+		<h1>Blog</h1>
+        <p>There's nothing here yet :)</p>
+	</div>
+</div>
+
+<style>
+</style>
