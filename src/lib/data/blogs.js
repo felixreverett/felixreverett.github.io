6300dd2319b38d0b7f43d803c0be53ed93cab5cf
Felix Everett <felixreverett@gmail.com>
2025-08-21 20:45:22 +0100

    description for blogs card

---
+++ src/lib/data/blogs.js
@@ -0,0 +1,10 @@
+export const blogs = [
+    {
+      id: 1,
+      imageSrc: '/images/wildfawn-app-screenshot.webp',
+      imageAlt: 'Screenshot for sprite batching blog',
+      title: 'Why We Use Sprite Batching in Graphics Programming',
+      description: 'My experience implementing sprite batching into Isola, and what it says about coding theory.',
+      blogLink: '/blog/sprite-batching',
+    }
+  ];
