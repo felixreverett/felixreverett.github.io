f4573a8209db236ee51c459fc2ebbe2ccc70e166
Felix Everett <felixreverett@gmail.com>
2025-10-03 10:45:05 +0100

    🪲 removal of broken link

--- src/lib/data/blogs.js
+++ src/lib/data/blogs.js
@@ -5,6 +5,6 @@ export const blogs = [
       imageAlt: 'Screenshot for sprite batching blog',
       title: 'Why We Use Sprite Batching in Graphics Programming',
       description: 'My experience implementing sprite batching into Isola, and what it says about coding theory.',
-      blogLink: '/blog/sprite-batching',
+      blogLink: '/blog',
     }
   ];
