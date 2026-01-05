870e70a8a34c2988cbbbeb384e10bfed9eaebc03
Felix Everett <felixreverett@gmail.com>
2026-01-05 12:59:36 +0000

    🧹 Svelte auto stuff

--- .svelte-kit/ambient.d.ts
+++ .svelte-kit/ambient.d.ts
@@ -35,7 +35,7 @@ declare module '$env/static/private' {
 	export const COMPUTERNAME: string;
 	export const ComSpec: string;
 	export const DriverData: string;
-	export const EFC_10920_1592913036: string;
+	export const EFC_24980_1592913036: string;
 	export const FPS_BROWSER_APP_PROFILE_STRING: string;
 	export const FPS_BROWSER_USER_PROFILE_STRING: string;
 	export const GIT_ASKPASS: string;
@@ -165,7 +165,7 @@ declare module '$env/dynamic/private' {
 		COMPUTERNAME: string;
 		ComSpec: string;
 		DriverData: string;
-		EFC_10920_1592913036: string;
+		EFC_24980_1592913036: string;
 		FPS_BROWSER_APP_PROFILE_STRING: string;
 		FPS_BROWSER_USER_PROFILE_STRING: string;
 		GIT_ASKPASS: string;
